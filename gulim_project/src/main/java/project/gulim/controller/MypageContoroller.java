e88a59472e4528bde5a9fda00ea867453c6ac706
DESKTOP-REI9PEB\kosmo <nahs001@naver.com>
2023-07-07 18:11:54 +0900

    7/7 나한솔 마이페이지컨트롤러-리엑트쪽 전송할 데이터 넘기는 매핑 작성

--- gulim_project/src/main/java/project/gulim/controller/MypageContoroller.java
+++ gulim_project/src/main/java/project/gulim/controller/MypageContoroller.java
@@ -1,12 +1,29 @@
 package project.gulim.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import project.gulim.domain.ChatingDTO;
+import project.gulim.service.MypageService;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequestMapping("/mypage")
 public class MypageContoroller {
+	
+	@Autowired
+	MypageService mypageService;
 
 	@RequestMapping("/{step}")
 	public String viewPage(@PathVariable String step) { // 페이지 이동(DB접속없는경우)
@@ -19,5 +36,35 @@ public class MypageContoroller {
 	@RequestMapping("/user_info/info_modify")
 	public String viewPage3() { // 페이지 이동(DB접속없는경우)
 		return "/mypage/user_info/info_modify";
+	}
+	
+	@RequestMapping(value = "/{room_num}", method = RequestMethod.GET)
+	@ResponseBody
+	public Map getRoomInfo(@PathVariable Integer room_num) {
+		String user_id = "nahs001";
+		
+		Map map = new HashMap();
+		List member_info = new ArrayList();
+		
+		HashMap room = mypageService.getRoomInfo(room_num);
+		map.putAll(room);
+		
+		String master_nickname = mypageService.getNicknameById((String)(room.get("id")));
+		map.put("master_nickname", master_nickname);
+		
+		List<Integer> join_list = mypageService.getJoinNumByRoomNum(room_num);
+		
+		for(Integer join_num : join_list) {
+			HashMap sheet = mypageService.getSheetByJoinNum(join_num);
+			String id = mypageService.getIdByJoinNum(join_num);
+			sheet.put("id", id);
+			member_info.add(sheet);
+		}
+		map.put("member_info", member_info);
+		map.put("user_id", user_id);
+		System.out.println(map.toString());
+		
+		
+		return map;
 	}
 }
---
+++ gulim_project/src/main/java/project/gulim/dao/MypageDAO.java
@@ -0,0 +1,19 @@
+package project.gulim.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import project.gulim.domain.ChatingDTO;
+
+@Mapper
+public interface MypageDAO {
+
+	public HashMap getRoomInfo(Integer room_num);
+	public List<Integer> getJoinNumByRoomNum(Integer room_num);
+	public HashMap getSheetByJoinNum(Integer join_num);
+	public String getIdByJoinNum(Integer join_num);
+	public String getNicknameById(String id);
+	
+}
