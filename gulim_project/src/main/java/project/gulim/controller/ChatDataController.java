994fafbfec80eabad549df978beb9cf78da4c97a
DESKTOP-REI9PEB\kosmo <nahs001@naver.com>
2023-07-18 19:49:45 +0900

    7/18 나한솔-리액트 및 데이터 전송부분 바꿈

--- gulim_project/src/main/java/project/gulim/controller/ChatDataController.java
+++ gulim_project/src/main/java/project/gulim/controller/ChatDataController.java
@@ -27,21 +27,48 @@ public class ChatDataController {
 	@GetMapping(value = "/room/{room_num}")
 	public Map getRoomInfo(@PathVariable Integer room_num) {
 		
+		// 채팅방에서 쓰일 전체 유저정보 및 방정보
 		Map map = new HashMap();
+		// 채팅방의 유저(게임마스터 제외) 정보
 		List member_info = new ArrayList();
-		
+		// 방정보를 가져와서 map에 담음
 		HashMap room = chatDataService.getRoomInfo(room_num);
 		map.putAll(room);
+		// 게임마스터의 메모를 모두 가져와서 map에 담음(메모가 있을 경우에만)
+		List<HashMap> master_memos = chatDataService.getMasterMemos(room_num);
+		if(master_memos.size() != 0) {
+			map.put("master_memos", master_memos);
+		}
 		
-		String master_nickname = chatDataService.getNicknameById((String)(room.get("id")));
-		map.put("master_nickname", master_nickname);
-		
+		// 채팅방에 참가하는 유저들 중 join_state가 1인(참가 허용) 유저의 join_num를 모두 가져옴
 		List<Integer> join_list = chatDataService.getJoinNumByRoomNum(room_num);
 		
 		for(Integer join_num : join_list) {
+			// 캐릭터시트 가져오기
 			HashMap sheet = chatDataService.getSheetByJoinNum(join_num);
+			// 해당 맴버의 id 가져오기
 			String id = chatDataService.getIdByJoinNum(join_num);
+			// 해당 캐릭터시트의 skill들 모두 가져오기
+			List<HashMap> skills = chatDataService.getSkillsBySheetNum((Integer)sheet.get("sheet_num"));
+			// 해당 캐릭터시트의 인벤토리와 그 안의 아이템을 모두 가져오기
+			HashMap inventory = chatDataService.getInventoryByJoinNum(join_num);
+			List<HashMap> items = chatDataService.getItemsByInvenNum((Integer)inventory.get("inventory_num"));
+			// 인벤토리에 아이템 넣기
+			inventory.put("items", items);
+			
+			// 캐릭터시트에 해당 유저의 id와 skills 넣기
 			sheet.put("id", id);
+			sheet.put("skills", skills);
+			// 캐릭터시트에 인벤토리 넣기
+			sheet.put("inventory", inventory);
+			
+			// 캐릭터시트에 해당 유저의 메모 넣기(메모가 있을 경우에만)
+			List<HashMap> memos = chatDataService.getMemosByJoinNum(join_num);
+			if(memos.size() != 0) {
+				sheet.put("memos", memos);
+			}
+			
+			
 			member_info.add(sheet);
 		}
 		map.put("member_info", member_info);
--- gulim_project/src/main/java/project/gulim/dao/ChatDataDAO.java
+++ gulim_project/src/main/java/project/gulim/dao/ChatDataDAO.java
@@ -13,7 +13,12 @@ public interface ChatDataDAO {
 	public HashMap getRoomInfo(Integer room_num);
 	public List<Integer> getJoinNumByRoomNum(Integer room_num);
 	public HashMap getSheetByJoinNum(Integer join_num);
+	public List<HashMap> getSkillsBySheetNum(Integer sheet_num);
 	public String getIdByJoinNum(Integer join_num);
 	public String getNicknameById(String id);
+	public HashMap getInventoryByJoinNum(Integer join_num);
+	public List<HashMap> getItemsByInvenNum(Integer inventory_num);
+	public List<HashMap> getMasterMemos(Integer room_num);
+	public List<HashMap> getMemosByJoinNum(Integer join_num);
 	
 }
